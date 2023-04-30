c6c84622ef74f9957a5657f6ffdc32797b66bff7
mounika <Mounika.Kalyadapu1@T-mobile.com>
2023-04-30 14:42:39 +0530

    "implemented instant search"

--- libs/books/feature/src/lib/book-search/book-search.component.spec.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.spec.ts
@@ -1,9 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedTestingModule } from '@tmo/shared/testing';
-
+import { clearSearch, searchBooks } from '@tmo/books/data-access';
 import { BooksFeatureModule } from '../books-feature.module';
 import { BookSearchComponent } from './book-search.component';
+import { of } from 'rxjs';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('ProductsListComponent', () => {
   let component: BookSearchComponent;
@@ -11,7 +13,7 @@ describe('ProductsListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [BooksFeatureModule, NoopAnimationsModule, SharedTestingModule]
+      imports: [ReactiveFormsModule, BooksFeatureModule, NoopAnimationsModule, SharedTestingModule]
     }).compileComponents();
   }));
 
@@ -21,7 +23,41 @@ describe('ProductsListComponent', () => {
     fixture.detectChanges();
   });
 
+  afterEach(()=>{
+    fixture.destroy();
+  })
+
   it('should create', () => {
     expect(component).toBeDefined();
   });
+  it('should call searchBooks',()=>{
+    const el = fixture.nativeElement.querySelector('input');
+    el.value = 'BlueJay';
+    el.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(component.searchBooks).toHaveBeenCalled();
+    });
+  });
+  it('should call searchBooks on formSubmit',()=>{
+    component.searchForm.controls.term.setValue('BlueJay');
+    const spy = spyOn(component['store'], 'dispatch')
+    component.searchBooks();
+    expect(spy).toHaveBeenCalledWith(
+      searchBooks({ term: 'BlueJay' })
+    );
+  });
+  it('should unsubscribe serachTermSubscription on onDestroy',()=>{
+    component.serachTermSubscription = of('1').subscribe(val=>val);
+    spyOn(component.serachTermSubscription, 'unsubscribe');
+    component.ngOnDestroy();
+    expect(component.serachTermSubscription.unsubscribe).toHaveBeenCalled();
+  });
+  it('should dispatch clearSearch on form Submit',()=>{
+    component.searchForm.value.term = "";
+    const storeSpy=spyOn(component['store'],'dispatch')
+      component.searchBooks();
+      expect(storeSpy).toHaveBeenCalledWith(clearSearch())
+
+    })
 });
